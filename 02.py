99a89b07ce79d0fc2e9825d60cd5a321faddc044
1am1337 <oskar.nitzschker@stud.hfm.eu>
2024-12-08 20:37:51 +0100

    st nur teil 1, bin mir nicht sicher wie ich teil 2 machen soll

--- 02.py
+++ 02.py
@@ -2,26 +2,32 @@ import re
 complete_file = None
 safe_reports = []
 safe_reports_temp = []
-with open("02test.txt") as file:
+increase_list = []
+with open("02.txt") as file:
     complete_file = file.read()
-
-lines = complete_file.split("\n")
-for i in range(len(lines)):
-    lines[i] = lines[i].split(" ")
-
-for i in range(len(lines)):
-    if lines[i] == sorted(lines[i]):
-        safe_reports_temp.append(lines[i])
-    elif lines[i] == sorted(lines[i], reverse = True):
-        safe_reports_temp.append(lines[i])
-    else:
-        pass # report is unsafe
+safe_reports_temp = complete_file.split("\n")
+for i in range(len(safe_reports_temp)):
+    safe_reports_temp[i] = safe_reports_temp[i].split(" ")
 
 for i in range(len(safe_reports_temp)):
     for j in range(len(safe_reports_temp[i])):
         safe_reports_temp[i][j] = int(safe_reports_temp[i][j])
 
-
 for i in range(len(safe_reports_temp)):
-    for j in range((len(safe_reports_temp[i]))):
-        if 
+    increase_list = []
+    if safe_reports_temp[i] == sorted(safe_reports_temp[i]) or safe_reports_temp[i] == sorted(safe_reports_temp[i], reverse = True):
+        safe_reports_temp[i] = sorted(safe_reports_temp[i])
+        for j in range((len(safe_reports_temp[i])-1)):
+            if safe_reports_temp[i][j+1] - safe_reports_temp[i][j] < 4 and safe_reports_temp[i][j+1] - safe_reports_temp[i][j] > 0:
+                increase_list.append(True)
+            else:
+                increase_list.append(False)
+        if False in increase_list:
+            continue
+        else:
+            safe_reports.append(safe_reports_temp[i])
+
+
+    
+print(safe_reports)
+print(len(safe_reports))
